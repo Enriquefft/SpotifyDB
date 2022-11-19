eaba65d477daaecc4ca62c1f78d3583bbe750cc9
Enriquefft <enriquefft2001@gmail.com>
2022-11-18 19:04:24 -0500

    first commit

---
+++ webapp/src/components/FooterBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="py-4 bg-white flex rounded-full justify-around">
+    <span class="px-16">SpotifyDB</span>
+    <ul class="flex justify-around w-full">
+      <li>About</li>
+      <li>Licence</li>
+      <li>
+        <a href="https://github.com/Enriquefft/SPDB-server"> Server source </a>
+      </li>
+      <li>Frontend source</li>
+    </ul>
+  </footer>
+</template>
+
+<style>
+ul li {
+  @apply mr-4 hover:underline;
+}
+</style>
